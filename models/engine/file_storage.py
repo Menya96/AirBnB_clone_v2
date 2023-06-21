20ef5a923d865904ea9eb643c27d9edb450af8f6
Abdi Berhe <abdi201380@gmail.com>
2023-06-21 09:02:51 -0700

    [Feauture]: Adding new method to delete and updating the all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,12 +10,13 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-	if cls:
-	    if isinstance(cls, str):
-		cls = eval(cls)
-	    return {k: v for k, v in FileStorage.__objects.items() if isinstance(v, cls)}
-
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            if isinstance(cls, str):
+                cls = eval(cls)
+            return {k: v for k, v in FileStorage.__objects.items(
+            ) if isinstance(v, cls)}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -50,15 +51,13 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-	"""Deletes an object if exists"""
-
-	if obj:
-	    key = obj.__class__.__name__ + "." + obj.id
-	    if key in FileStorage.__objects:
-		del FileStorage.__objects[key]
-
+        '''Deletes an object from __objects'''
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]

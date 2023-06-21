96c8638c09f7f034faacd9ea16d4d8b716b26d47
Abdi Berhe <abdi201380@gmail.com>
2023-06-21 10:57:26 -0700

    [Update]: Considering file storage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
+from models.city import City
+from models import storage_type
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(
+            String(128), nullable=False
+            ) if storage_type == 'db' else ''
+
+    if storage_type == 'db':
+        cities = relationship(
+                'City',
+                cascade='all, delete, delete-orphan',
+                backref='state'
+                )
+    else:
+        @property
+        def cities(self):
+            """Returns the cities in this State"""
+            from models import storage
+            cities_in_state = []
+            for value in storage.all(City).values():
+                if value.state_id == self.id:
+                    cities_in_state.append(value)
+            return cities_in_state

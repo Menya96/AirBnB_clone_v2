1f71b0ceb3f57082c9eaaac0019b3c46c0acb4ab
Abdi Berhe <abdi201380@gmail.com>
2023-07-06 14:28:58 +0000

    [Update]: Check if the versions directory exists or the archive file has been created

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,20 +4,18 @@ Module: '1-pack_web_static'
 Script that generates a .tgz archive from contents of web_static folder
 '''
 
-from fabric.api import local
 from datetime import datetime
-import os.path
+from fabric.api import local
+from os.path import isdir
 
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder"""
-
+    """generates a tgz archive"""
     date = datetime.now().strftime("%Y%m%d%H%M%S")
-
-    local("mkdir -p versions")
-    archive_name = "versions/web_static_{}.tgz".format(date)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -czvf {} web_static".format(archive_name)).failed is True:
+    if isdir("versions") is False:
+        local("mkdir versions")
+    file_name = "versions/web_static_{}.tgz".format(date)
+    local("tar -cvzf {} web_static".format(file_name))
+    if file_name:
+        return file_name
+    else:
         return None
-    return archive_name

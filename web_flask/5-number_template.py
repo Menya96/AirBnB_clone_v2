ca553e4dbd94b2cb5cc8ae7b1f19153f77590038
Abdi Berhe <abdi201380@gmail.com>
2023-07-22 14:23:55 -0700

    [Script Update]: pass variable to html using jinja template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+'''
+Module: '5-number_template'
+script that starts a Flask web application
+'''
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_fun(text):
+    text = text.replace('_', ' ')
+    return 'C ' + text
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    text = text.replace('_', ' ')
+    return 'Python ' + text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num_route(n):
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_template(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)

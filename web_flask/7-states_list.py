a2e29b87eb1ab8209a57eb004e79247e5bba56e1
Abdi Berhe <abdi201380@gmail.com>
2023-07-23 08:36:59 -0700

    [Script & File]: script that starts a Flask web application

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,18 +8,17 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_session(exception):
-    """ Remove the current SQLAlchemy Session after each request """
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ Display a HTML page with the list of all State objects """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    states = storage.all('State')
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def flask_teardown(exc):
+    '''Remove the current SQLALchemy Session after each request'''
+    storage.close()
 
 
 if __name__ == '__main__':

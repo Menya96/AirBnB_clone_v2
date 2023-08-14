6d33b1617d294c798004d457c2bd77280789c75e
Abdi Berhe <abdi201380@gmail.com>
2023-08-14 01:00:37 -0700

    [Update]: Gives correct output

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,10 +10,9 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ Display a HTML page with the list of all State objects """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    '''The states_list page.'''
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,15 +13,15 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-def states():
-    """Displays an HTML page with a list of all States.
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states and related cities.
 
-    States are sorted by name.
+    States/cities are sorted by name.
     """
     states = storage.all("State")
-    return render_template("9-states.html", state=states)
-
+    return render_template("8-cities_by_states.html", states=states)
+'''
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
@@ -30,7 +30,7 @@ def states_id(id):
         if state.id == id:
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
-
+'''
 
 @app.teardown_appcontext
 def teardown(exc):
@@ -39,4 +39,4 @@ def teardown(exc):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port="5000")

7a42f545e7a5d4f60e184e8ebe82e701b82c3071
Abdi Berhe <abdi201380@gmail.com>
2023-08-14 01:09:09 -0700

    [Update]: Gives correct output

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -21,16 +21,7 @@ def cities_by_states():
     """
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
-'''
 
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
-'''
 
 @app.teardown_appcontext
 def teardown(exc):
